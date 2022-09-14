4b6df6cc0a22eb8af80887db9e9d796a5f00be88
oleg_name <_olg@mail.ru>
2022-09-14 13:34:51 +0300

    ex005 if

---
+++ ex005_if/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter user name: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША");   
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
