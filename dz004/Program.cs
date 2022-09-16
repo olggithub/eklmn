1723cce1fa0a4a4b18770c24923b0f3f840d5e1a
oleg_name <_olg@mail.ru>
2022-09-16 14:34:25 +0300

    dz004 задача 8

---
+++ dz004/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 8
+int i = 0;
+Console.Clear();
+Console.Write("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a<0)
+           {
+            i=a;
+            a=0;
+           }
+
+Console.Write("Результат : ");
+while(i<=a)
+           {
+int ost = i%2;
+if(ost == 0)
+            {
+             Console.Write(i+", "); 
+             i++;
+            }
+else        
+            {
+             i++;
+            }    
+           }
