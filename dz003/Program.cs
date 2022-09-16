6afc4511c4c4d88ad0e377f18ce076249338986d
oleg_name <_olg@mail.ru>
2022-09-16 13:44:39 +0300

    dz003 задача 6

---
+++ dz003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 6
+Console.Clear();
+Console.Write("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int ost = a%2;
+if(ost == 0)
+            {
+             Console.WriteLine("Число является четным"); 
+            }
+else        
+            {
+             Console.WriteLine("Число является нечетным");
+            }    
