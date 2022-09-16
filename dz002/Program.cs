b87b9bf4a976dc7a9d97184a45b45edfc1cca5a7
oleg_name <_olg@mail.ru>
2022-09-16 13:24:06 +0300

    dz002

---
+++ dz002/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 4
+int i=0;
+Console.Clear();
+Console.Write("Введите число a : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b : ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число с : ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+while (i<3)
+           {
+             if(max < b)
+                        {
+                          max = b;
+                        }
+             if(max < c)
+                        {
+                          max = c;
+                        }      
+            i++;              
+           }
+
+Console.WriteLine("max = "+max);
