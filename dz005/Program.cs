61d5c8e7101b51bb55876bd08a81bfe08bb19775
oleg_name <_olg@mail.ru>
2022-09-16 14:50:08 +0300

    dz005 задача 8 исправленная

---
+++ dz005/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 8
+int i = 1;
+Console.Clear();
+Console.Write("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a>0)
+           {
+            
+Console.Write("Результат : ");
+while(i<=a)
+           {
+int ost = i%2;
+if(ost == 0)
+            {
+             Console.Write(i+", "); 
+             i++;
+            }
+else        
+            {
+             i++;
+            }    
+           }
+           }
+ else
+      {
+        Console.Write("Число должно быть больше 0");
+      }          
