5e1540c9834f3691be30c38e62a1bc24de9a23c2
oleg_name <_olg@mail.ru>
2022-09-16 12:56:30 +0300

    dz01

---
+++ dz001/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите число a : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b : ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b)
+           {
+            Console.WriteLine("min = max = "+a);
+           }
+else
+           {
+            if(a<b)
+                   {
+                    Console.WriteLine("min = "+a);
+                    Console.WriteLine("max = "+b);
+                   }
+             else
+                   {
+                    Console.WriteLine("min = "+b);
+                    Console.WriteLine("max = "+a);
+                   }
+    
+           }
