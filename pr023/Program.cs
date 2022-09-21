ec902ceaaf40d7f7e9ea5e7b7d75cbc20bc695e1
oleg_name <_olg@mail.ru>
2022-09-21 17:22:13 +0300

    pr023 lesson2 ex12

---
+++ pr023/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число 1: ");
+
+int ch1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int ch2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (ch1 % ch2 == 0)
+                  {
+                    Console.WriteLine("числа кратны");
+                  }
+else
+                  {
+                    Console.WriteLine("числа не кратны");
+                    Console.WriteLine("остаток от деления равен "+ch1 % ch2);
+                  }                  
