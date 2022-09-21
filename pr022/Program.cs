eefe39ae37ec5256f27b8a47e30d33e9bb2081c9
oleg_name <_olg@mail.ru>
2022-09-21 16:51:34 +0300

    pro22 pr02 №11

---
+++ pr022/Program.cs
@@ -0,0 +1,9 @@
+﻿int x = new Random().Next(100,999);
+Console.WriteLine(x);
+int ost = x % 10;
+Console.WriteLine(ost);
+int des = (x-ost) % 100;
+Console.WriteLine(des);
+int res = (x-des-ost)/10+ost;
+Console.WriteLine(res);
+ 
