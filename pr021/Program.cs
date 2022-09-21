e5a5950b99ea3d7f731ffb89c193dc02020ff3c7
oleg_name <_olg@mail.ru>
2022-09-21 16:23:59 +0300

    pr021 практика 2 задача 1

---
+++ pr021/Program.cs
@@ -0,0 +1,16 @@
+﻿int x = new Random().Next(10,99);
+Console.WriteLine(x);
+int ost = x % 10;
+int des = (x-ost)/10;
+if(des == ost)
+                {
+                 Console.WriteLine(ost);
+                }
+else if(des>ost)             
+                {
+                 Console.WriteLine(des);
+                }
+else
+                {
+                 Console.WriteLine(ost);
+                }
