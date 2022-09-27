52c440b7c63338b0828966f499e6efe13095b4fe
oleg_name <_olg@mail.ru>
2022-09-27 21:14:17 +0300

    pr042_e24

---
+++ pr042_e24/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 24
+// Написать программу, которая принимает на вход число А и выдает сумму чисел от 1 до А
+
+Console.Clear();
+Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+int result;
+
+int summa_a(int number)
+{
+ int summa_a = 0;
+ int i=1;
+ while (i<=number)
+ { 
+    summa_a=summa_a+i;
+    i++;
+ }  
+ return summa_a;
+}
+
+result = summa_a(a);
+Console.WriteLine($"Сумма чисел равна : {result}");
