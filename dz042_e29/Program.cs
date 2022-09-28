c08305ac0d5e80bbe940f29ad9b6c6e96fe9c61a
oleg_name <_olg@mail.ru>
2022-09-28 14:57:58 +0300

    dz042_e29

---
+++ dz042_e29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//      6, 1, 33 -> [6, 1, 33]
+
+int i = 0;
+int n = 8;
+int [] m = new int [n];
+
+Console.Clear();
+Console.WriteLine("Массив из восьми элементов:");
+Console.Write("[");
+
+for(i = 0; i < 8; i++)
+                      {
+                        if (i < n-1)
+                                    {
+                                      m[i] = new Random().Next(1,100);
+                                      Console.Write($"{m[i]}, ");
+                                    }
+                        else         
+                                    {
+                                      m[i] = new Random().Next(1,100);
+                                      Console.Write($"{m[i]}");
+                                    }                                 
+                      }
+Console.Write("]");
